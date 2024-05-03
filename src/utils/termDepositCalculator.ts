4b5a9288c6e918ec9847250d9eca89c58b2ddc91
Henrique Pureza <henrique.pureza13@gmail.com>
2024-05-03 17:09:36 +1000

    refactor: add comments and clean code

--- src/utils/termDepositCalculator.ts
+++ src/utils/termDepositCalculator.ts
@@ -1,10 +1,15 @@
+const MONTHS_IN_YEAR = 12;
+const QUARTERS_IN_YEAR = 4;
+
 enum InterestPaymentFrequency {
   Monthly = "monthly",
   Quarterly = "quarterly",
   Yearly = "yearly",
   AtMaturity = "atMaturity",
 }
-
+/**
+ * Determines the frequency of interest compounding based on the payment frequency and investment term.
+ */
 const getInterestCompoundingFrequency = (
   interestPaymentFrequency: InterestPaymentFrequency,
   investmentTermInYears: number
@@ -13,8 +18,8 @@ const getInterestCompoundingFrequency = (
     InterestPaymentFrequency,
     number | ((termInYears: number) => number)
   > = {
-    [InterestPaymentFrequency.Monthly]: 12,
-    [InterestPaymentFrequency.Quarterly]: 4,
+    [InterestPaymentFrequency.Monthly]: MONTHS_IN_YEAR,
+    [InterestPaymentFrequency.Quarterly]: QUARTERS_IN_YEAR,
     [InterestPaymentFrequency.Yearly]: 1,
     [InterestPaymentFrequency.AtMaturity]: (termInYears: number) =>
       1 / termInYears,
@@ -33,6 +38,17 @@ const getInterestCompoundingFrequency = (
   throw new Error("Invalid interest payment frequency");
 };
 
+/**
+ * Calculates the compound interest for a given investment.
+ *
+ * A = P * (1 + r/n)^(nt)
+ * Where:
+ * A is the amount of money accumulated after n years, including interest.
+ * P is the principal amount (the initial sum of money).
+ * r is the annual interest rate (decimal).
+ * n is the number of times that interest is compounded per year.
+ * t is the number of years the money is invested or borrowed for.
+ */
 const calculateCompoundInterest = (
   principal: number,
   annualInterestRate: number,
@@ -48,11 +64,14 @@ const calculateCompoundInterest = (
   );
 };
 
+/**
+ * Calculates the term of the investment in years from the current date to the given end date.
+ */
 const calculateInvestmentTermInYears = (endDateInput: MonthInput): number => {
   const startDate = new Date();
   const endDate = new Date(endDateInput);
   const investmentTermInMonths =
-    (endDate.getFullYear() - startDate.getFullYear()) * 12 +
+    (endDate.getFullYear() - startDate.getFullYear()) * MONTHS_IN_YEAR +
     (endDate.getMonth() - startDate.getMonth());
 
   if (investmentTermInMonths <= 0) {
@@ -61,7 +80,7 @@ const calculateInvestmentTermInYears = (endDateInput: MonthInput): number => {
     );
   }
 
-  return investmentTermInMonths / 12;
+  return investmentTermInMonths / MONTHS_IN_YEAR;
 };
 
 const validateInputs = (
@@ -82,6 +101,14 @@ const validateInputs = (
   }
 };
 
+/**
+ * Calculates the final balance of the investment including compound interest.
+ * @param {number} initialDeposit - The initial deposit amount.
+ * @param {MonthInput} endDateInput - The end date of the investment.
+ * @param {number} annualInterestRate - The annual interest rate.
+ * @param {InterestPaymentFrequency} interestPaymentFrequency - The frequency at which interest is paid.
+ * @returns {string} The final balance formatted as a string with two decimal places.
+ */
 const calculateFinalBalance = (
   initialDeposit: number,
   endDateInput: MonthInput,
