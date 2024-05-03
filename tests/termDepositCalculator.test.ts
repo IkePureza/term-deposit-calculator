4e74e00588ccf99803b3d818fb7d0f48ee92b063
Henrique Pureza <henrique.pureza13@gmail.com>
2024-05-03 16:14:01 +1000

    feat: implement at maturity interest calculation

--- tests/termDepositCalculator.test.ts
+++ tests/termDepositCalculator.test.ts
@@ -1,14 +1,39 @@
 import { describe, expect, it } from "vitest";
-import { calculateFinalBalance } from "../utils/termDepositCalculator";
+import {
+  calculateFinalBalance,
+  InterestPaymentFrequency,
+} from "../utils/termDepositCalculator";
 
 describe("Calculate final balance", () => {
   it("calculates final balance with monthly interest payments", () => {
     const startDeposit = 10000;
     const investmentTermInYears = 3;
     const interestRate = 1.1;
+    const interestPaymentFrequency = InterestPaymentFrequency.Monthly;
 
     expect(
-      calculateFinalBalance(startDeposit, investmentTermInYears, interestRate)
+      calculateFinalBalance(
+        startDeposit,
+        investmentTermInYears,
+        interestRate,
+        interestPaymentFrequency
+      )
     ).toBe("10335.35");
+  });
+
+  it("calculates final balance with interest paid at maturity", () => {
+    const startDeposit = 10000;
+    const investmentTermInYears = 3;
+    const interestRate = 1.1;
+    const interestPaymentFrequency = InterestPaymentFrequency.AtMaturity;
+
+    expect(
+      calculateFinalBalance(
+        startDeposit,
+        investmentTermInYears,
+        interestRate,
+        interestPaymentFrequency
+      )
+    ).toBe("10330.00");
   });
 });
--- utils/termDepositCalculator.ts
+++ utils/termDepositCalculator.ts
@@ -1,24 +1,74 @@
-const getInterestCompoundingFrequency = (): number => {
-  return 12;
+enum InterestPaymentFrequency {
+  Monthly = "monthly",
+  Quarterly = "quarterly",
+  Yearly = "yearly",
+  AtMaturity = "atMaturity",
+}
+
+const getInterestCompoundingFrequency = (
+  interestPaymentFrequency: InterestPaymentFrequency,
+  investmentTermInYears: number
+): number => {
+  const frequencyMap: Record<
+    InterestPaymentFrequency,
+    number | ((termInYears: number) => number)
+  > = {
+    [InterestPaymentFrequency.Monthly]: 12,
+    [InterestPaymentFrequency.Quarterly]: 4,
+    [InterestPaymentFrequency.Yearly]: 1,
+    [InterestPaymentFrequency.AtMaturity]: (termInYears: number) =>
+      1 / termInYears,
+  };
+
+  const frequency = frequencyMap[interestPaymentFrequency];
+
+  if (typeof frequency === "function") {
+    return frequency(investmentTermInYears);
+  }
+
+  if (frequency) {
+    return frequency;
+  }
+
+  throw new Error("Invalid interest payment frequency");
+};
+
+const calculateCompoundInterest = (
+  principal: number,
+  annualInterestRate: number,
+  compoundingFrequency: number,
+  investmentTermInYears: number
+): number => {
+  const interestRatePerPeriod = annualInterestRate / compoundingFrequency;
+  const numberOfCompoundingPeriods =
+    compoundingFrequency * investmentTermInYears;
+
+  return (
+    principal * Math.pow(1 + interestRatePerPeriod, numberOfCompoundingPeriods)
+  );
 };
 
 const calculateFinalBalance = (
   initialDeposit: number,
   investmentTermInYears: number,
-  annualInterestRate: number
+  annualInterestRate: number,
+  interestPaymentFrequency: InterestPaymentFrequency
 ): string => {
-  const interestCompoundingFrequency = getInterestCompoundingFrequency();
+  const interestCompoundingFrequency = getInterestCompoundingFrequency(
+    interestPaymentFrequency,
+    investmentTermInYears
+  );
 
   const decimalInterestRate = annualInterestRate / 100;
 
-  const finalBalance =
-    initialDeposit *
-    Math.pow(
-      1 + decimalInterestRate / interestCompoundingFrequency,
-      interestCompoundingFrequency * investmentTermInYears
-    );
+  const finalBalance = calculateCompoundInterest(
+    initialDeposit,
+    decimalInterestRate,
+    interestCompoundingFrequency,
+    investmentTermInYears
+  );
 
   return finalBalance.toFixed(2);
 };
 
-export { calculateFinalBalance };
+export { calculateFinalBalance, InterestPaymentFrequency };
